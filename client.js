c5a18fcba833bd0778d61f87968fd018a38b5295
talialino <talia-apiu@hotmail.com>
2023-07-05 03:20:39 -0300

    change code for the client and server if comunicate

--- client.js
+++ client.js
@@ -9,23 +9,43 @@ const socket = io(serverURL)
 
 // Evento de conexão estabelecida
 socket.on("connect", () => {
-	console.log("Conectado ao servidor.")
+	console.log("Conexão estabelecida com o servidor.")
 
-	// Enviar um arquivo para o servidor
-	const file = "file.txt" // Arquivo que deseja enviar
-	const tolerance = 3 // Valor de tolerância para o arquivo
+	// Modo depósito
+	const fileToDeposit = "arquivo.txt"
+	const tolerance = 3
 
-	const fileData = {
-		file: file,
-		tolerance: tolerance,
-	}
+	socket.emit("depositFile", { file: fileToDeposit, tolerance })
 
-	socket.emit("depositFile", fileData)
-})
+	socket.on("depositFileResponse", (response) => {
+		console.log("Resposta do modo depósito:", response)
+	})
 
-// Evento para receber a resposta do servidor
-socket.on("depositFileResponse", (response) => {
-	console.log("Resposta do servidor:", response)
+	// Modo recuperação
+	const fileToRetrieve = "arquivo.txt"
+
+	socket.emit("retrieveFile", fileToRetrieve)
+
+	socket.on("retrieveFileResponse", (fileStream) => {
+		if (fileStream) {
+			const destinationPath = `./${fileToRetrieve}`
+
+			fileStream.pipe(fs.createWriteStream(destinationPath))
+			console.log(`Arquivo ${fileToRetrieve} recuperado com sucesso.`)
+		} else {
+			console.log(`Arquivo ${fileToRetrieve} não encontrado.`)
+		}
+	})
+
+	// Atualizar número de réplicas
+	const fileToUpdate = "arquivo.txt"
+	const newTolerance = 5
+
+	socket.emit("updateReplicas", { filename: fileToUpdate, tolerance: newTolerance })
+
+	socket.on("updateReplicasResponse", (response) => {
+		console.log("Resposta da atualização de réplicas:", response)
+	})
 })
 
 // Evento de desconexão
--- server.js
+++ server.js
@@ -1,32 +1,186 @@
 const fs = require("fs")
-const multer = require("multer")
-const express = require("express")
 const http = require("http")
 const path = require("path")
 const socketIO = require("socket.io")
 
-const app = express()
-const server = http.createServer(app)
+const server = http.createServer()
 const io = socketIO(server)
 const port = 3000
-
-// Configuração do Multer para upload de arquivos
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, "uploads/")
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.originalname)
-	},
-})
-
-const upload = multer({ storage: storage })
 
 // Mapa para rastrear o número de réplicas de cada arquivo
 const replicasMap = new Map()
 
+// Rota para o modo depósito
+io.on("connection", (socket) => {
+	console.log("Novo cliente conectado.")
+
+	// Evento para depósito de arquivo
+	socket.on("depositFile", (fileData) => {
+		try {
+			const { file, tolerance } = fileData
+
+			if (!file || !tolerance || tolerance <= 0) {
+				throw new Error(
+					"Requisição inválida. Certifique-se de enviar um arquivo e um valor válido para a tolerância."
+				)
+			}
+
+			const encodedFilename = encodeURIComponent(file)
+
+			// Verificar se o número de réplicas foi alterado
+			if (replicasMap.has(file)) {
+				const previousTolerance = replicasMap.get(file)
+
+				if (tolerance > previousTolerance) {
+					// Aumentar a quantidade de réplicas
+					const diff = tolerance - previousTolerance
+					const directories = getAvailableDirectories()
+
+					for (let i = 0; i < diff; i++) {
+						const directory = directories[i % directories.length]
+						const destinationPath = path.join(directory, encodedFilename)
+
+						mkdirSyncRecursive(directory) // Criar diretório, se não existir
+
+						fs.copyFile(file, destinationPath, (err) => {
+							if (err) {
+								console.error(err)
+							} else {
+								console.log(`Arquivo copiado para ${destinationPath}`)
+							}
+						})
+					}
+				} else if (tolerance < previousTolerance) {
+					// Diminuir a quantidade de réplicas
+					const diff = previousTolerance - tolerance
+					const directories = getAvailableDirectories()
+					const filesToRemove = []
+
+					directories.forEach((directory) => {
+						const filePath = path.join(directory, file)
+						if (fs.existsSync(filePath)) {
+							filesToRemove.push(filePath)
+						}
+					})
+
+					for (let i = 0; i < diff && i < filesToRemove.length; i++) {
+						const fileToRemove = filesToRemove[i]
+						fs.unlink(fileToRemove, (err) => {
+							if (err) {
+								console.error(err)
+							} else {
+								console.log(`Arquivo removido: ${fileToRemove}`)
+							}
+						})
+					}
+				}
+			} else {
+				// Criar réplicas iniciais
+				const directories = getAvailableDirectories()
+
+				for (let i = 0; i < tolerance; i++) {
+					const directory = directories[i % directories.length]
+					const destinationPath = path.join(directory, encodedFilename)
+
+					mkdirSyncRecursive(directory) // Criar diretório, se não existir
+
+					fs.copyFile(file, destinationPath, (err) => {
+						if (err) {
+							console.error(err)
+						} else {
+							console.log(`Arquivo copiado para ${destinationPath}`)
+						}
+					})
+				}
+			}
+
+			// Atualizar o número de réplicas no mapa
+			replicasMap.set(file, tolerance)
+
+			// Enviar resposta para o cliente
+			socket.emit("depositFileResponse", "Arquivo armazenado com sucesso!")
+		} catch (err) {
+			console.error(err)
+			// Enviar resposta de erro para o cliente
+			socket.emit("depositFileResponse", "Erro ao armazenar o arquivo.")
+		}
+	})
+
+	// Evento para recuperação de arquivo
+	socket.on("retrieveFile", (filename) => {
+		try {
+			if (!filename) {
+				throw new Error("Nome do arquivo não fornecido na solicitação.")
+			}
+
+			// Lógica para encontrar o arquivo em algum dos locais replicados
+			const directories = getAvailableDirectories()
+
+			let found = false
+
+			directories.forEach((directory) => {
+				const filePath = path.resolve(directory, filename)
+				if (fs.existsSync(filePath)) {
+					const fileStream = fs.createReadStream(filePath)
+					socket.emit("retrieveFileResponse", fileStream)
+					found = true
+				}
+			})
+
+			if (!found) {
+				// Enviar resposta de arquivo não encontrado para o cliente
+				socket.emit("retrieveFileResponse", null)
+			}
+		} catch (err) {
+			console.error(err)
+			// Enviar resposta de erro para o cliente
+			socket.emit("retrieveFileResponse", null)
+		}
+	})
+
+	// Evento para atualização de réplicas
+	socket.on("updateReplicas", (data) => {
+		try {
+			const { filename, tolerance } = data
+
+			if (!filename || !tolerance || tolerance <= 0) {
+				throw new Error(
+					"Requisição inválida. Certifique-se de fornecer um nome de arquivo e um valor válido para a tolerância."
+				)
+			}
+
+			if (replicasMap.has(filename)) {
+				const previousTolerance = replicasMap.get(filename)
+
+				if (tolerance !== previousTolerance) {
+					// Atualizar o número de réplicas para o arquivo
+					replicasMap.set(filename, tolerance)
+					socket.emit("updateReplicasResponse", "Número de réplicas atualizado com sucesso!")
+				} else {
+					socket.emit("updateReplicasResponse", "O número de réplicas já é o mesmo solicitado.")
+				}
+			} else {
+				socket.emit("updateReplicasResponse", "Arquivo não encontrado.")
+			}
+		} catch (err) {
+			console.error(err)
+			socket.emit("updateReplicasResponse", "Erro ao atualizar o número de réplicas.")
+		}
+	})
+
+	// Desconectar cliente
+	socket.on("disconnect", () => {
+		console.log("Cliente desconectado.")
+	})
+})
+
+// Função auxiliar para obter os diretórios disponíveis
+function getAvailableDirectories() {
+	return ["directory1/", "directory2/", "directory3/"] // quantidade de diretórios diferentes fornecidos
+}
+
 // Criar diretórios caso não existam
-const mkdirSyncRecursive = (directory) => {
+function mkdirSyncRecursive(directory) {
 	const pathParts = directory.replace(/\/$/, "").split("/")
 
 	pathParts.reduce((parentDir, childDir) => {
@@ -40,155 +194,7 @@ const mkdirSyncRecursive = (directory) => {
 	}, "")
 }
 
-// Rota para o modo depósito
-app.post("/deposit", upload.single("file"), (req, res, next) => {
-	try {
-		const file = req.file
-		const tolerance = parseInt(req.body.tolerance)
-
-		if (!file || !tolerance || tolerance <= 0) {
-			throw new Error(
-				"Requisição inválida. Certifique-se de enviar um arquivo e um valor válido para a tolerância."
-			)
-		}
-
-		const encodedFilename = encodeURIComponent(file.originalname)
-
-		// Verificar se o número de réplicas foi alterado
-		if (replicasMap.has(file.originalname)) {
-			const previousTolerance = replicasMap.get(file.originalname)
-
-			if (tolerance > previousTolerance) {
-				// Aumentar a quantidade de réplicas
-				const diff = tolerance - previousTolerance
-				const directories = getAvailableDirectories()
-
-				for (let i = 0; i < diff; i++) {
-					const directory = directories[i % directories.length]
-					const destinationPath = directory + encodedFilename
-
-					mkdirSyncRecursive(directory) // Criar diretório, se não existir
-
-					fs.copyFile(file.path, destinationPath, (err) => {
-						if (err) {
-							console.error(err)
-						} else {
-							console.log(`Arquivo copiado para ${destinationPath}`)
-						}
-					})
-				}
-			} else if (tolerance < previousTolerance) {
-				// Diminuir a quantidade de réplicas
-				const diff = previousTolerance - tolerance
-				const directories = getAvailableDirectories()
-				const filesToRemove = []
-
-				directories.forEach((directory) => {
-					const filePath = directory + file.originalname
-					if (fs.existsSync(filePath)) {
-						filesToRemove.push(filePath)
-					}
-				})
-
-				for (let i = 0; i < diff && i < filesToRemove.length; i++) {
-					const fileToRemove = filesToRemove[i]
-					fs.unlink(fileToRemove, (err) => {
-						if (err) {
-							console.error(err)
-						} else {
-							console.log(`Arquivo removido: ${fileToRemove}`)
-						}
-					})
-				}
-			}
-		} else {
-			// Criar réplicas iniciais
-			const directories = getAvailableDirectories()
-
-			for (let i = 0; i < tolerance; i++) {
-				const directory = directories[i % directories.length]
-				const destinationPath = directory + encodedFilename
-
-				mkdirSyncRecursive(directory) // Criar diretório, se não existir
-
-				fs.copyFile(file.path, destinationPath, (err) => {
-					if (err) {
-						console.error(err)
-					} else {
-						console.log(`Arquivo copiado para ${destinationPath}`)
-					}
-				})
-			}
-		}
-
-		// Atualizar o número de réplicas no mapa
-		replicasMap.set(file.originalname, tolerance)
-
-		res.send("Arquivo armazenado com sucesso!")
-	} catch (err) {
-		next(err)
-	}
-})
-
-// Rota para o modo recuperação
-app.get("/retrieve", (req, res, next) => {
-	try {
-		const filename = req.query.filename
-
-		if (!filename) {
-			throw new Error("Nome do arquivo não fornecido na solicitação.")
-		}
-
-		// Lógica para encontrar o arquivo em algum dos locais replicados
-		const directories = getAvailableDirectories()
-
-		let found = false
-
-		directories.forEach((directory) => {
-			const filePath = path.resolve(directory, filename)
-			if (fs.existsSync(filePath)) {
-				res.sendFile(filePath)
-				found = true
-			}
-		})
-
-		if (!found) {
-			res.status(404).send("Arquivo não encontrado")
-		}
-	} catch (err) {
-		next(err)
-	}
-})
-
-// Middleware de tratamento de erros
-app.use((err, req, res, next) => {
-	console.error(err)
-	res.status(500).send("Ocorreu um erro durante o processamento da solicitação.")
-})
-
-// Função auxiliar para obter os diretórios disponíveis
-function getAvailableDirectories() {
-	return ["directory1/", "directory2/", "directory3/"] // quantidade de diretórios diferentes fornecidos
-}
-
-// Iniciar o servidor e o socket.io
+// Iniciar o servidor
 server.listen(port, () => {
 	console.log(`Servidor rodando na porta ${port}`)
-})
-
-io.on("connection", (socket) => {
-	console.log("Novo cliente conectado.")
-
-	// Escutar eventos personalizados
-	socket.on("customEvent", (data) => {
-		console.log("Evento personalizado recebido:", data)
-	})
-
-	// Emitir eventos personalizados
-	socket.emit("customEvent", "Dados do evento personalizado")
-
-	// Desconectar cliente
-	socket.on("disconnect", () => {
-		console.log("Cliente desconectado.")
-	})
 })
